6f2ad54162c9156ecf9904cf934cc561897898f8
1 <bondarenko-1994alenka@mail.ru>
2023-04-14 21:09:14 +0300

    Task2

--- Task2.java
+++ Task2.java
@@ -6,28 +6,27 @@ import java.io.FileNotFoundException;
 import java.util.*;
 import java.io.IOException;
 
-
 public class Task2 {
-   public static void main(String[] args) {
-      HashMap<String, Integer> nameCount = new HashMap<>();
-      try (Scanner scanner = new Scanner(new File("phoneBook.txt"))) {
-         while (scanner.hasNextLine()) {
-            String line = scanner.nextLine();
-            String[] fi = line.split(" ");
-            String name = fi[0].trim();
-            Integer count = nameCount.getOrDefault(name, 0);
-            nameCount.put(name, count + 1);
-         }
-      }  catch (IOException e) {
-        e.printStackTrace();
+    public static void main(String[] args) {
+        HashMap<String, Integer> nameCount = new HashMap<>();
+        try (Scanner scanner = new Scanner(new File("phoneBook.txt"))) {
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                String[] fi = line.split(" ");
+                String name = fi[0].trim();
+                Integer count = nameCount.getOrDefault(name, 0);
+                nameCount.put(name, count + 1);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        List<Map.Entry<String, Integer>> sortList = new ArrayList<>(nameCount.entrySet());
+        sortList.sort(Collections.reverseOrder(Map.Entry.comparingByValue()));
+        System.out.println("Имена повторяются следующее количество раз:");
+        for (Map.Entry<String, Integer> entry : sortList) {
+            if (entry.getValue() > 1) {
+                System.out.println(entry.getKey() + "-" + entry.getValue());
+            }
+        }
     }
-      List<Map.Entry<String, Integer>> sortList = new ArrayList<>(nameCount.entrySet());
-      sortList.sort(Collections.reverseOrder(Map.Entry.comparingByValue()));
-      System.out.println("Имена повторяются следующее количество раз:");
-      for (Map.Entry<String, Integer> entry : sortList) {
-         if (entry.getValue() > 1) {
-            System.out.println(entry.getKey() + "-" + entry.getValue());
-         }
-      }
-   }
 }
