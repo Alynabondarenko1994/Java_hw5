863098f57b484a716d376420ecee40fb77bd1e84
1 <bondarenko-1994alenka@mail.ru>
2023-04-14 21:09:22 +0300

    Task1

--- Task1.java
+++ Task1.java
@@ -1,22 +1,21 @@
 
 // Реализуйте структуру телефонной книги с помощью HashMap, учитывая, что 1 человек может иметь несколько телефонов.
 
-
 import java.util.*;
 import java.io.*;
 
-public class Task1{
-   
-    public static void addNumber(Map<String, ArrayList<Integer>> map){
-       
+public class Task1 {
+
+    public static void addNumber(Map<String, ArrayList<Integer>> map) {
+
         Scanner sc = new Scanner(System.in);
         System.out.print("Введите фамилию: ");
         String key = sc.nextLine().trim();
         System.out.print("Введите телефон: ");
         int value = sc.nextInt();
-        
+
         if (map.containsKey(key)) {
-            map.get(key).add((int)value);
+            map.get(key).add((int) value);
         } else {
             ArrayList<Integer> list = new ArrayList<>();
             list.add(value);
@@ -25,18 +24,15 @@ public class Task1{
 
     }
 
-    public static void printBook(Map<String, ArrayList<Integer>> map){
+    public static void printBook(Map<String, ArrayList<Integer>> map) {
         for (var item : map.entrySet()) {
             String phones = "";
-            for(int el: item.getValue()){
+            for (int el : item.getValue()) {
                 phones = phones + el + ", ";
             }
             System.out.printf("%s: %s \n", item.getKey(), phones);
         }
     }
-
-
-
 
     public static void main(String[] args) {
 
@@ -50,29 +46,20 @@ public class Task1{
             System.out.println("3 - Выйти");
 
             choice = sc.nextInt();
-            if (choice > 3){
+            if (choice > 3) {
                 System.out.println("Введите цифру от 1 до 3");
-             } 
-            switch (choice) {          
-               case 1:            
-                addNumber(bookPhone);
-                  break;
-               case 2:
-               printBook(bookPhone);
-                  break;
-            };
+            }
+            switch (choice) {
+                case 1:
+                    addNumber(bookPhone);
+                    break;
+                case 2:
+                    printBook(bookPhone);
+                    break;
+            }
+            ;
 
         } while (choice != 3);
-      
-       
-    
-      }
+
     }
-
-
-
-
-
-       
-       
-       
+}
